2ab467efc3e16ca05d65f0ff0aa8a2c3fa0acdb8
maxinu2005 <polco098@gmail.com>
2025-11-24 17:49:30 -0300

    arreglamos el issue del proyecto 2 que no habiamos visto 😢

--- TrabajoPractico_2/proyecto_2/modules/implementacion.py
+++ TrabajoPractico_2/proyecto_2/modules/implementacion.py
@@ -58,6 +58,12 @@ class Temperatura_DB():
     def borrar_temperatura(self, fechastr):
         fecha = self.convertir_fecha(fechastr)
         self.arbol.eliminar(fecha)
+    def cargar_archivo(self, nombre_archivo):
+        with open(nombre_archivo, 'r') as archivo:
+            for linea in archivo:
+                fecha_str, temp_str = linea.strip().split(';')
+                temperatura = float(temp_str)
+                self.guardar_temperatura(temperatura, fecha_str)
     
 
 
@@ -65,28 +71,19 @@ class Temperatura_DB():
 db = Temperatura_DB() # Crear una instancia de la base de datos de temperaturas
 
 # Guardar algunas temperaturas con fechas específicas
-db.guardar_temperatura(25.5, "01/01/2023")
-db.guardar_temperatura(27.8, "02/01/2023")
-db.guardar_temperatura(30.2, "03/01/2023")
-db.guardar_temperatura(22.1, "04/01/2023")
-db.guardar_temperatura(24.0, "05/01/2023")
-db.guardar_temperatura(26.5, "06/01/2023")
-db.guardar_temperatura(28.3, "07/01/2023")
-db.guardar_temperatura(29.9, "08/01/2023")
-db.guardar_temperatura(23.4, "09/01/2023")
-db.guardar_temperatura(21.0, "10/01/2023")
+db.cargar_archivo('proyecto_2/docs/muestras.txt')
 
 # Se muestran las temperaturas guardadas
 print("Temperaturas guardadas:")
 db.devolver_todas_temperaturas()
 
 # Consulta en fecha específica
-fecha = "08/01/2023"
+fecha = "08/01/2025"
 print(f"Temperatura en la fecha {fecha}:", db.devolver_temperatura(fecha), '°C')
 
 # Consultas en rango de fechas
-fecha_inicio = "04/01/2023"
-fecha_fin = "09/01/2023"
+fecha_inicio = "04/01/2025"
+fecha_fin = "09/01/2025"
 print("Temperaturas en rango:")
 db.devolver_temperaturas(fecha_inicio, fecha_fin)
 print("Temperatura máxima en rango:", db.max_temp_rango(fecha_inicio, fecha_fin), '°C')
@@ -97,8 +94,8 @@ print("Temperaturas extremas en rango:", db.temp_extremos_rango(fecha_inicio, fe
 print("Cantidad de muestras:", db.cantidad_muestras())
 
 # Borrar algunas temperaturas
-db.borrar_temperatura("08/01/2023")
-db.borrar_temperatura("04/01/2023")
+db.borrar_temperatura("08/01/2025")
+db.borrar_temperatura("04/01/2025")
 
 # Se muestran las temperaturas después de borrar algunas
 print("Temperaturas después de borrar:")
